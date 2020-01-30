67aeeb841fc80eb16e74691a37ad5bd5672d5436
Genelet Php <genelet@gmail.com>
2020-01-30 11:33:27 -0500

    no more gate, use access

--- src/access.php
+++ src/access.php
@@ -75,6 +75,34 @@ class Access extends Base
 		return $this->Encode(join("/", array($ip, $login, $str_group, $when, $hash)));
     }
 
+    public function Forbid(): string
+    {
+        $escaped = urlencode($_SERVER["REQUEST_URI"]);
+        $this->Set_cookie_session($this->go_probe_name, $escaped);
+        $this->Set_cookie_expire($this->role_obj->surface);
+        $oauth = "";
+        $default = "";
+        $first = "";
+        foreach ($this->role_obj->issuers as $k => $issuer) {
+			if ($this->Is_oauth1($k) || $this->Is_oauth2($k)) {
+                if (empty($oauth)) {$oauth = $k;}
+            } else {
+                if (empty($first)) {$first = $k;}
+                if ($issuer->default) {
+                    $default = $k;
+                }
+            }
+        }
+        if (!empty($default)) {$first = $default;}
+        $redirect = $this->script . "/" . $this->Role_name . "/" . $this->Tag_name . "/";
+		$redirect .= empty($first) ? $oauth : $this->login_name;
+        $redirect .= "?" .  $this->go_uri_name . "=" . $escaped . "&" . $this->go_err_name . "=1025";
+		if (!empty($first)) {
+			$redirect .= "&" . $this->provider_name . "=$first";
+		}
+        return $redirect;
+    }
+
     public function Verify_cookie(string ...$raws): ?Gerror
     {
         $role = $this->role_obj;
--- src/filter.php
+++ src/filter.php
@@ -3,7 +3,7 @@ declare (strict_types = 1);
 
 namespace Genelet;
 
-class Filter extends Gate
+class Filter extends Access
 {
 	public $ARGS;
 
